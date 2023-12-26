0180b67a7c29a1192146f5594be262a2ee38ffc3
Louis Le Cam <louislecam25@gmail.com>
2023-12-26 17:41:59 +0100

    Added actions

---
+++ crates/editor_action/src/lib.rs
@@ -0,0 +1,64 @@
+use editor_document::Document;
+
+macro_rules! actions {
+    (
+        enum Actions {
+            $($variant_name:ident => enum $enum_name:ident |$($fn_arg:ident : $fn_arg_type:ty),*| {
+                $($name:ident $(($($arg_name:ident : $arg_type:ty),+))? $code:block)*
+            })+
+        }
+    ) => {
+        pub enum Action {
+            $($variant_name($enum_name),)+
+        }
+
+        impl Action {
+            pub fn as_str(&self) -> &'static str {
+                match self {
+                    $(Self::$variant_name(action) => action.as_str(),)*
+                }
+            }
+        }
+
+        $(
+            pub enum $enum_name {
+                $($name $(($($arg_type)+))?,)*
+            }
+
+            #[allow(unused_variables)]
+            impl $enum_name {
+                pub fn execute(&self, $($fn_arg: $fn_arg_type),*) {
+                    match self {
+                        $(Self::$name $(($($arg_name),+))? => $code)*
+                    }
+                }
+
+                pub fn as_str(&self) -> &'static str {
+                    paste::paste!{
+                        match self {
+                            $(Self::$name $(($($arg_name),+))? => stringify!($name:snake),)*
+                        }
+                    }
+                }
+            }
+        )+
+    };
+}
+
+actions! {
+    enum Actions {
+        Command => enum Command | | {
+            Quit { todo!() }
+        }
+        Document => enum DocumentAction |document: &mut Document| {
+            MoveLeft { document.move_left(); }
+            MoveRight { document.move_right(); }
+            MoveUp { document.move_up(); }
+            MoveDown { document.move_down(); }
+            DeleteBefore { document.delete_before() }
+            Insert(char: char) { document.insert(*char) }
+            Write { document.write() }
+            InsertLineBeforeCursor { document.insert_line_before_cursor() }
+        }
+    }
+}
